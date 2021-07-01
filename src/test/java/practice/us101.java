4f5b2b50b7dff418298fff15905f8068a7f9e08c
Ramin <raminrocker75@gmail.com>
2021-06-30 19:31:12 -0500

    us101 - work1

---
+++ src/test/java/practice/us101.java
@@ -0,0 +1,7 @@
+package practice;
+
+public class us101 {
+    //awesome work has been done here , and ready to push to remote
+    //so i can get my code reviewed and merged into the master in the codebase
+
+}
